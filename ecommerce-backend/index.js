8a684d988aafc97f94522c93225a237d559fc0de
aplloshahriar <aaronshahriar.as@gmail.com>
2024-08-13 00:00:50 +0600

    first commit

---
+++ ecommerce-backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+env.config();
+
+// database
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@ecommerce-databse.figbt.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("databse connected");
+  })
+  .catch((error) => {
+    console.log("database connection error:", error);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+  console.log(`server running on ${process.env.PORT}`);
+});
